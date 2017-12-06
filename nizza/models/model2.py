56cba60f351a8235b5a95cce6a9e4bb1c91e4133
fstahlberg <fs439@jabba>
2017-12-06 23:29:31 +0000

    Remove debug messages

--- nizza/models/model2.py
+++ nizza/models/model2.py
@@ -33,35 +33,6 @@ import tensorflow as tf
 from tensorflow.contrib.training.python.training import hparam
 
 
-def _print_shape_py(t, msg):
-  print("%s shape: %s" % (msg, t.shape))
-  return sum(t.shape)
-
-
-def print_shape(t, msg="", dtype=tf.float32):
-  """Print shape of the tensor for debugging."""
-  add = tf.py_func(_print_shape_py, [t, msg], tf.int64)
-  shp = t.get_shape()
-  ret = t + tf.cast(tf.reduce_max(add) - tf.reduce_max(add), dtype=dtype)
-  ret.set_shape(shp)
-  return ret
-
-
-def _print_data_py(t, msg):
-  print("%s shape: %s" % (msg, t.shape))
-  print("%s data: %s" % (msg, t))
-  return sum(t.shape)
-
-
-def print_data(t, msg="", dtype=tf.float32):
-  """Print shape and content of the tensor for debugging."""
-  add = tf.py_func(_print_data_py, [t, msg], tf.int64)
-  shp = t.get_shape()
-  ret = t + tf.cast(tf.reduce_max(add) - tf.reduce_max(add), dtype=dtype)
-  ret.set_shape(shp)
-  return ret
-
-
 def register_hparams_sets():
   base = hparam.HParams(
     lex_hidden_units=[512, 512, 512],
